7211db073348944d6761f67d6a6a988f49b15985
Ayush <ayushmanojgupta24@gmail.com>
2020-06-12 11:08:27 +0530

    Added Board Reset Functionality

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,2 +1,22 @@
+#!/bin/bash -x
 echo "=================WELCOME TO TIC_TAC_TOE SIMULATION================"
+#declaring a dictonary
+declare -A board
 
+#function to reset board
+function boardReset ()
+{
+	local row=0
+	local column=0
+	#resetting each and every row and column of the board matrix
+	for(( row=1; row<=3; row++))
+	do
+		for(( column=1; column<=3; column++))
+		do
+			board[$row,$column]=" "
+		done
+	done
+}
+
+#calling board reset function
+boardReset
